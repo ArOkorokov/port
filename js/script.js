1f314c4717536a82ae566f6fe6770b12a06a11c1
ArOkorokov <arOkorokov@gmail.com>
2023-11-25 03:09:19 -0800

    first commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,37 @@
 'use strict'
 
 
-let a = document.querySelector('.nav');
-let b = document.querySelector('.header-burger');
-let c = document.querySelector('body');
 
-b.addEventListener('click', () => {
-    a.classList.toggle('burger-active');
-    c.classList.toggle('lock');
-})
+
+function burger() {
+    let a = document.querySelector('.nav');
+    let b = document.querySelector('.header-burger');
+    let c = document.querySelector('body');
+    
+    b.addEventListener('click', (e) => {
+        a.classList.toggle('burger-active');
+        c.classList.toggle('lock');
+        e.stopPropagation();
+    })
+}
+burger();
+
+this.onload = function() {
+    gsap.from('.main-header--losung-wrapper', {
+        opacity: 0,
+        duration: 4,
+        delay: 0.3,
+    })
+    gsap.from('.main-header_developer-develop_wrapper', {
+        opacity: 0,
+        x: -150,
+        duration: 3,
+        delay: 0.3,
+    })
+    gsap.from('.main-header_developer-web_wrapper' , {
+        opacity: 0,
+        x: -150,
+        duration: 2,
+        rotation: -10,
+    })
+}
